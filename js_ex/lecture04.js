d8522996c1c3f7e8b7bee5a900ff1a54824111f0
효정이 <w2029@e-mirim.hs.kr>
2022-01-22 21:49:48 +0900

    js_ex

---
+++ js_ex/lecture04.js
@@ -0,0 +1,8 @@
+var a=100, b=3.14;
+var c="안녕하세요", d="a";
+var e=true, f=false;
+
+//typeof는 어떤 타입 인지 알려준다.
+console.log(a,typeof(a)); // ex) 100 "number"
+console.log(c,typeof(c)); // ex) 안녕하세요 "string"
+console.log(e,typeof(e)); // ex) false "boolean"
