66d2fd826fa01535353de3d4ff00a2bb029dc084
효정이 <w2029@e-mirim.hs.kr>
2022-01-22 22:22:41 +0900

    if문 예제

---
+++ js_ex/example2.js
@@ -0,0 +1,6 @@
+if(ture){
+    console.log("1");
+}
+else{
+    console.log("2");
+}
---
+++ js_ex/example3.js
@@ -0,0 +1,12 @@
+if(ture){
+    console.log("1");
+}
+else if(ture){
+    console.log("2");
+}
+else if(ture){
+    console.log("3");
+}
+else{
+    console.log("4");
+}
