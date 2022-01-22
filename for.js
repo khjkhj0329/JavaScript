09db8315da805f3acbe52dc4fc1f58eaecb7b21a
효정이 <w2029@e-mirim.hs.kr>
2022-01-22 23:03:10 +0900

    반복문

---
+++ for.js
@@ -0,0 +1,19 @@
+// while문
+// var array = [1,2,3,4,5,6,7,8,9,10];
+
+// var i = 0;
+// while(i < array.length){
+//     //반복 실행될 코드
+//     console.log(array[i]);
+
+//     i++; //반복문 코드가 한번 실행되고 나면 실행할 업데이트 구문
+// }
+
+// for문
+var array = [1,2,3,4,5,6,7,8,9,10];
+
+for(var i = 0; i <array.length; i++){ //조건식
+
+    // 반복 실행될 코드
+    console.log(array[i]);
+}
